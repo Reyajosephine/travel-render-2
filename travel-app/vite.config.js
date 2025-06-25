ecd2d640502e1a5e60af054ddfaf179d785e289a
Reyajosephine <reyacelene@gmail.com>
2025-06-25 23:22:29 +0530

    vite-config.js

--- travel-app/vite.config.js
+++ travel-app/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:5173,
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
+  server: {
+    port: process.env.PORT || 5173,
+    host: true
+  },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true
   }
-})
+});
